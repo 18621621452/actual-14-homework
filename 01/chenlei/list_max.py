aefcc14602db96301b05c8bf546978255dad43c5
anzerchen <anzerchen@163.com>
2017-05-08 15:20:21 +0800

    chenlei homework

--- 01/chenlei/list_max.py
+++ 01/chenlei/list_max.py
@@ -2,16 +2,6 @@ num_list = [1,666666666,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,655
 first = 0
 second = 0 
 
-'''
-for i in num_list :
-    if i > first :
-        first = i
-for i in num_list :
-    if i > second :
-       if i != first :
-            second = i
-'''
-
 for i in num_list :
     for j in num_list :
         if j > first :
